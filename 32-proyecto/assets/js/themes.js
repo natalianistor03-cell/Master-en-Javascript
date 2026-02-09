d79fc1867e3a1d35c5210f807ecb0626717b1ed3
Natalia Nistor <natalianistor03@gmail.com>
2026-02-09 09:10:44 +0100

    ProyectoJS

---
+++ 32-proyecto/assets/js/themes.js
@@ -0,0 +1,22 @@
+// Cambiar tema
+
+let themeGreen = document.querySelector(".theme__green");
+let themeRed = document.querySelector(".theme__red");
+let themeBlue = document.querySelector(".theme__blue");
+
+let page = document.querySelector("html");
+
+themeGreen.addEventListener("click", () => {
+    page.removeAttribute("class");
+    page.classList.add("theme__green--active");
+});
+
+themeRed.addEventListener("click", () => {
+    page.removeAttribute("class");
+    page.classList.add("theme__red--active");
+});
+
+themeBlue.addEventListener("click", () => {
+    page.removeAttribute("class");
+    page.classList.add("theme__blue--active");
+});
